a896174cf5461f5430b12ffeb0e3cad0a3300bd7
tak <njirtak@gmail.com>
2019-01-07 18:18:31 +0900

    feat: finish SignInComponent

--- react-firebase-auth/src/index.js
+++ react-firebase-auth/src/index.js
@@ -6,10 +6,10 @@ import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 import Firebase, { FirebaseContext } from './components/Firebase';
 
-ReactDOM.render(
+ReactDOM.render((
     <FirebaseContext.Provider value={new Firebase()}>
         <App />
-    </FirebaseContext.Provider>,
+    </FirebaseContext.Provider>),
     document.getElementById('root')
 );
 
