ea6c8cabd367d51c911a86faee9dc6e8a8bdfa8c
tak <njirtak@gmail.com>
2019-01-09 16:53:22 +0900

    chore: delete unused code

--- react-firebase-auth/src/components/App/index.js
+++ react-firebase-auth/src/components/App/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from '../Navigation';
-import LandingPage from '../Landing';
+// import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
 import SignInPage from '../SignIn';
 import PasswordForgetPage from '../PasswordForget';
-import HomePage from '../Home';
+// import HomePage from '../Home';
 // import AdminPage from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
@@ -20,9 +20,9 @@ const App = () => (
 
             <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
             <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-            <Route exact path={ROUTES.LANDING} component={LandingPage} />
+            {/* <Route exact path={ROUTES.LANDING} component={LandingPage} /> */}
             <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-            <Route path={ROUTES.HOME} component={HomePage} />
+            {/* <Route path={ROUTES.HOME} component={HomePage} /> */}
             {/* <Route path={ROUTES.ACCOUNT} component={AccountPage} /> */}
             {/* <Route path={ROUTES.ADMIN} component={AdminPage} /> */}
         </div>
